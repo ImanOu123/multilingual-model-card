cdfa8c472a0f0d99f33865c108d9c25b194b7859
Iman Ouzzani <iouzzani@babel-login-3.lti.cs.cmu.edu>
2024-07-10 09:26:24 -0400

    some more term processing

--- src/dictionary_collection/process_growing_dict.py
+++ src/dictionary_collection/process_growing_dict.py
@@ -1,5 +1,10 @@
 import json
 import re
+from pyinflect import getAllInflections
+import nltk
+
+def remove_punct(term):
+    return re.sub(r'[^0-9a-zA-Z /]+', "", term.lower()).replace("  ", " ").strip()
 
 def extract_paper_term_lst(jsonl_path):
     with open(jsonl_path) as f:
@@ -20,7 +25,7 @@ def remove_paper_unique_terms(paper_term_lst):
     for dct in paper_term_lst:
         for term in dct["terms"]:
             # if seeing a term for a second or more time add to the term_lst
-            if term in temp_term_lst and term not in term_lst:
+            if term in temp_term_lst and term not in term_lst and term.strip() != "":
                 term_lst.append(term)
             # if haven't seen term before, add to temp_term_lst
             elif term not in temp_term_lst:
@@ -42,7 +47,7 @@ def read_list_from_file(file_path):
 def remove_terms_w_abbr(term_lst):
 
     def check_abbr(term):
-        if re.search(r"([A-Z]\.*){2,}s?", term) or term == "": 
+        if re.search(r"([A-Z]\.*){2,}s?", term) or len(remove_punct(term)) <= 2: 
             return False 
         else:
             return True
@@ -50,12 +55,80 @@ def remove_terms_w_abbr(term_lst):
     # remove terms with abbreviations
     return list(filter(check_abbr, term_lst))
 
-def remove_terms_w_nonce():
+def remove_terms_w_nonce(term_lst):
     # remove terms with nonce words
     
-    return
+    # remove any terms after terms starting with z and before terms
+    # starting with number 0 in sorted list
+    term_lst.sort()
+    
+    postZidx = len(term_lst)
+    firstNumidx = 0
+    
+    for idx, term in enumerate(term_lst):
+        if idx != len(term_lst)-1 and term != "":
+            if term[0] == 'z' and term_lst[idx+1][0] != 'z':
+                # index of term after last term starting with z
+                postZidx = idx+1
+        
+        if idx != 0:
+            if term[0] == '0' and term_lst[idx-1][0] != '0':
+                # index of first term starting with 0
+                firstNumidx = idx
+             
+    return term_lst[firstNumidx:postZidx]
+    
+def remove_term_inflections(term_lst):
+    term_lst.sort()
+    
+    def flatten(lsts):
+        tmp = []
+        for lst in lsts:
+            for e in lst:
+                tmp.append(e)
+        return tmp
+    
+    tmp_term_lst = []
+    processed_term_lst = []
+    inflect_lst = []
+    
+    for term in term_lst:
+        
+        termWOpunct = remove_punct(term)
+        # if the term or an inflection of the term wasn't seen before
+        if termWOpunct.replace(" ", "") not in tmp_term_lst:
+                
+            # add to tmp_term_lst without punctuation to use as a check
+            tmp_term_lst.append(termWOpunct.replace(" ", ""))
+            
+            # finds inflections of final word in term
+            inflect_lst = flatten(list(map(lambda x: list(x), getAllInflections(termWOpunct.split()[-1]).values())))
+            
+            # add inflections of full term to tmp_term_lst 
+            tmp_term_lst += list(map(lambda lastWord: (" ". join(termWOpunct.split()[:-1]) + " " + lastWord).replace(" ", ""), inflect_lst))
+            
+            # add to final term list 
+            processed_term_lst.append(term)
+    
+    return processed_term_lst
 
-
+def remove_non_nouns(term_lst):
+    
+    processed_term_lst = []
+    
+    for term in term_lst:
+        
+        # https://stackoverflow.com/questions/33587667/extracting-all-nouns-from-a-text-file-using-nltk
+        is_noun = lambda pos: pos[:2] == 'NN'
+        tokenized = nltk.word_tokenize(term)
+        nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+        if nouns != []:
+            # print(term, nltk.pos_tag(tokenized))
+            processed_term_lst.append(term)
+    
+    return processed_term_lst
+            
+        
 if __name__ == "__main__":
     
     # paper_term_lst = extract_paper_term_lst('growing_dict/terms.jsonl')
@@ -68,7 +141,11 @@ if __name__ == "__main__":
     # ----------------------------------------------------------------------
 
     term_lst = read_list_from_file('growing_dict/terms.txt')
-    term_lst_wo_abbr = remove_terms_w_abbr(term_lst)
-    term_lst_wo_abbr.sort()
-    write_list_to_file('growing_dict/processed_terms.txt', term_lst_wo_abbr)
     
+    term_lst_wo_abbr = remove_terms_w_abbr(term_lst)
+    term_lst_wo_nonce = remove_terms_w_nonce(term_lst_wo_abbr)
+    term_lst_wo_repeat = remove_term_inflections(term_lst_wo_nonce)
+    term_lst_w_noun = remove_non_nouns(term_lst_wo_repeat)
+    
+    write_list_to_file('growing_dict/processed_terms.txt', term_lst_w_noun)
+    print(remove_punct("10-fold cross-validation") == remove_punct("10-fold cross-validation"))
